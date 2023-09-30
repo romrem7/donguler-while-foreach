880504b5a471320aea81b7141c44f1cd6b89a7b4
romrem7 <127850740+romrem7@users.noreply.github.com>
2023-09-30 20:20:56 +0300

    Add files via upload

---
+++ donguler-while-foreach/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //While
+        // 1'den başlayarak console dan girilen sayıya kadar (sayı dahil) ortalama hesaplayıp console a yazdıran program.
+        // Console.Write("Bir sayı giriniz : ");
+        // int sayac = 1;
+        // int toplam = 0;
+        // int sayi = int.Parse(Console.ReadLine());
+        // while (sayac <= sayi)
+        // {
+        //     toplam += sayac;
+        //     sayac ++;
+        // }
+        // Console.WriteLine("Ortalama: " + toplam/sayi);
+
+        // // a'dan z'ye kadar tüm harfleri console a yazdır.
+        // char character = 'a';
+        // while (character < 'z')
+        // {
+        //     Console.Write(character);
+        //     character ++;
+        // }
+
+        Console.WriteLine("****** Foreach ******");
+        string[] arabalar = {"BMW","Ford","Toyota","Nissan"};
+        foreach (var araba in arabalar)
+        {
+            Console.WriteLine(araba);
+        }
+    }
+}
